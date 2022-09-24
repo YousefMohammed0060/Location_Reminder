70c3e50548df0caedeb9e232a0b9e51923836ba3
myous <myousef049@gmail.com>
2022-09-24 11:53:17 +0200

    Initial commit

---
+++ app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
@@ -0,0 +1,56 @@
+package com.udacity.project4.locationreminders
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.udacity.project4.R
+import com.udacity.project4.authentication.AuthenticationActivity
+import com.udacity.project4.databinding.ActivityReminderDescriptionBinding
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
+import org.koin.android.ext.android.inject
+
+/**
+ * Activity that displays the reminder details after the user clicks on the notification
+ */
+class ReminderDescriptionActivity : AppCompatActivity() {
+
+    val viewModel: ReminderDescriptionViewModel by inject()
+
+    companion object {
+        private const val EXTRA_ReminderDataItem = "EXTRA_ReminderDataItem"
+
+        //        receive the reminder object after the user clicks on the notification
+        fun newIntent(context: Context, reminderDataItem: ReminderDataItem): Intent {
+            val intent = Intent(context, ReminderDescriptionActivity::class.java)
+            intent.putExtra(EXTRA_ReminderDataItem, reminderDataItem)
+            return intent
+        }
+    }
+
+    private lateinit var binding: ActivityReminderDescriptionBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_reminder_description
+        )
+
+        binding.reminderDataItem = intent.getSerializableExtra(EXTRA_ReminderDataItem) as ReminderDataItem?
+
+        binding.deleteButton.setOnClickListener{
+            viewModel.deleteReminder(binding.reminderDataItem!!)
+            finish()
+        }
+
+        binding.editButton.setOnClickListener{
+
+            val intent = Intent(this, RemindersActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.putExtra("reminderId", binding.reminderDataItem!!)
+            startActivity(intent)
+        }
+
+    }
+}
